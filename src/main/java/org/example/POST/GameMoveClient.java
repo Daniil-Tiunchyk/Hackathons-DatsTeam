3a9e2f2ad5ba93e8b2fcad429617bcac28106c2a
vladik234 <vlad.stavrov2004.stavrov@mail.ru>
2024-10-12 11:47:58 +0300

    feat: add main script

--- src/main/java/org/example/POST/GameMoveClient.java
+++ src/main/java/org/example/POST/GameMoveClient.java
@@ -2,6 +2,11 @@ package org.example.POST;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.example.Script.MoveScript;
+import org.example.Script.ShieldScript;
+import org.example.Script.ShootScript;
+import org.example.models.move.GameState;
+import org.example.models.move.MoveResponse;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -14,49 +19,53 @@ public class GameMoveClient {
     private static final String AUTH_TOKEN = "67038c0234b8867038c0234b8a";
 
     // Метод для отправки POST-запроса с пустым массивом и получения ответа
-    public static String sendMoveRequest(String serverUrl) throws IOException {
-        URL url = new URL(serverUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
+    public static String sendMoveRequest(String serverUrl, MoveResponse moveRequest) throws IOException {
 
-        // Устанавливаем заголовки
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestProperty("X-Auth-Token", AUTH_TOKEN);
-        connection.setDoOutput(true);
 
-        // Преобразуем объект MoveRequest с пустым массивом в JSON
-        Gson gson = new GsonBuilder().create();
 
-        String jsonInputString = gson.toJson(new MoveRequest());
+            URL url = new URL(serverUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
 
-        try (OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), StandardCharsets.UTF_8)) {
-            writer.write(jsonInputString);
-            writer.flush();
-        }
+            // Устанавливаем заголовки
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("X-Auth-Token", AUTH_TOKEN);
+            connection.setDoOutput(true);
 
-        int responseCode = connection.getResponseCode();
+            // Преобразуем объект MoveRequest с пустым массивом в JSON
+            Gson gson = new GsonBuilder().create();
 
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-            StringBuilder content = new StringBuilder();
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
+            String jsonInputString = gson.toJson(moveRequest);
+
+            try (OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), StandardCharsets.UTF_8)) {
+                writer.write(jsonInputString);
+                writer.flush();
             }
-            in.close();
 
-            return content.toString();
-        } else {
-            // Если сервер вернул ошибку, читаем и возвращаем её
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getErrorStream(), StandardCharsets.UTF_8));
-            StringBuilder content = new StringBuilder();
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+                StringBuilder content = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+
+                return content.toString();
+            } else {
+                // Если сервер вернул ошибку, читаем и возвращаем её
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getErrorStream(), StandardCharsets.UTF_8));
+                StringBuilder content = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                throw new RuntimeException("Ошибка: " + responseCode + " - " + content);
             }
-            in.close();
-            throw new RuntimeException("Ошибка: " + responseCode + " - " + content);
-        }
+
     }
 
     // Метод для форматированного вывода JSON
@@ -83,23 +92,42 @@ public class GameMoveClient {
         String serverUrl = "https://games-test.datsteam.dev/play/magcarp/player/move";
 
         try {
-            // Отправляем запрос с пустым массивом и получаем ответ
-            String response = sendMoveRequest(serverUrl);
+            while(true){
 
-            System.out.println("Ответ сервера:");
-            System.out.println(prettyPrintJson(response));
+            // Отправляем запрос с пустым массивом и получаем ответ
+            String response = sendMoveRequest(serverUrl, new MoveResponse());
+            Gson gson = new Gson();
+                GameState gameState = gson.fromJson(response, GameState.class);
+            MoveResponse moveResponse = new MoveResponse();
+
+            ShootScript shootScript = new ShootScript();
+            ShieldScript shieldScript = new ShieldScript();
+
+            moveResponse = MoveScript.processGameState(gameState);
+
+            moveResponse.setTransports(shootScript.shoot(moveResponse.getTransports(),gameState));
+            moveResponse.setTransports(shieldScript.shieldSuitability(moveResponse.getTransports(),gameState));
+            response = sendMoveRequest(serverUrl, moveResponse);
+            //System.out.println("Ответ сервера:");
+            //System.out.println(prettyPrintJson(response));
 
 
             // Сохраняем ответ в файл в корневой папке проекта
             String filePath = System.getProperty("user.dir") + "/response.json";
+            String fileRequestPath = System.getProperty("user.dir") + "/request.json";
             saveJsonToFile(response, filePath);
 
-            System.out.println("JSON-ответ сохранен в файл: " + filePath);
+            saveJsonToFile(gson.toJson(moveResponse), fileRequestPath);
 
+           // System.out.println("JSON-ответ сохранен в файл: " + filePath);
+                Thread.sleep(1000);
+            }
         } catch (IOException e) {
             System.err.println("Произошла ошибка при выполнении запроса: " + e.getMessage());
         } catch (RuntimeException e) {
             System.err.println(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/org/example/Script/ShieldScript.java
+++ src/main/java/org/example/Script/ShieldScript.java
@@ -4,6 +4,7 @@ package org.example.Script;
 import org.example.POST.Transport;
 import org.example.models.move.Enemy;
 import org.example.models.move.GameState;
+import org.example.models.move.TransportAction;
 import org.example.models.move.TransportResponse;
 
 import java.util.List;
@@ -16,13 +17,13 @@ public class ShieldScript {
     private final static int MINIMAL_HEALTH_WOUT_SHIELD = 30;
 
 
-    public List<Transport> shieldSuitability(List<Transport> transports, GameState gameState) {
+    public List<TransportAction> shieldSuitability(List<TransportAction> transports, GameState gameState) {
         return transports.stream()
                 .map(transport -> decideToActivate(transport, gameState)) // Обновляем каждый транспорт
                 .collect(Collectors.toList());
     }
 
-    private Transport decideToActivate(Transport transport, GameState gameState) {
+    private TransportAction decideToActivate(TransportAction transport, GameState gameState) {
 
         TransportResponse currentTransport = getCurrentTransport(transport, gameState);
         transport.setActivateShield(checkShieldSuitability(currentTransport, gameState));
@@ -55,7 +56,7 @@ public class ShieldScript {
         return Math.sqrt(Math.pow(enemy.getX() - currentTransport.getX(), 2) + Math.pow(enemy.getY() - currentTransport.getY(), 2));
     }
 
-    private TransportResponse getCurrentTransport(Transport transport, GameState gameState) {
+    private TransportResponse getCurrentTransport(TransportAction transport, GameState gameState) {
         return gameState.getTransports().stream()
                 .filter(t1 -> t1.getId().equals(transport.getId()))
                 .findFirst()
