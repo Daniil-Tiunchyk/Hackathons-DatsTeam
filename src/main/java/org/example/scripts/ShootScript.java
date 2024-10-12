7a9ed05115ede336e4dc8f64b2bce19e8cccda4d
vladik234 <vlad.stavrov2004.stavrov@mail.ru>
2024-10-12 15:45:48 +0300

    feat: update speed index.html

--- src/main/java/org/example/scripts/ShootScript.java
+++ src/main/java/org/example/scripts/ShootScript.java
@@ -72,7 +72,7 @@ public class ShootScript {
 
         //TODO Заменить на <100
         // 3) Если врагов 4 или больше, действуем в зависимости от здоровья
-        if (currentHealth < 100) {
+        if (currentHealth <= 100) {
             for (Enemy enemy : enemies) {
                 Attack newAttack = getAttack(enemies, currentTransport, attackRange, attackExplosionRadius, enemy);
                 if (newAttack != null) return newAttack;
